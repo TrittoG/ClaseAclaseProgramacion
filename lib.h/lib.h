9894c3f1e5bff387bd688f1d80815906803ce6a5
TrittoG <36174147+TrittoG@users.noreply.github.com>
2018-04-10 21:21:15 -0300

    Librerias necesarias para las clases

---
+++ lib.h/lib.h
@@ -0,0 +1,6 @@
+#ifndef LIB_H_INCLUDED
+#define LIB_H_INCLUDED
+
+int getInt(char *mensaje, char *error, int intentos, int maximo, int minimo, int *resultado);
+float getFloat(char *mensaje, char *error, int intentos, float maximo, float minimo, float *resultado);
+#endif // LIB_H_INCLUDED
